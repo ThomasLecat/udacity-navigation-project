36f982f598e85efad581fdab00d83952a2a13e2a
Thomas Lecat <t.lecat@instadeep.com>
2021-03-31 10:53:28 +0200

    fix(replay buffer): sampled indexes out of the buffer's range

--- navigation/replay_buffer.py
+++ navigation/replay_buffer.py
@@ -18,14 +18,16 @@ class ReplayBufferInterface:
 
     def sample(self, num_samples: int):
         sampled_indexes = np.random.choice(
-            self.buffer_indexes, size=num_samples, p=self.probabilities()
+            self.buffer_indexes[: len(self.buffer)],
+            size=num_samples,
+            p=self.probabilities(),
         )
         samples = [self.buffer[idx] for idx in sampled_indexes]
-        observations = np.stack([s["observation"] for s in samples], axis=0)
-        actions = np.array([s["action"] for s in samples], dtype=np.int)
-        rewards = np.array([s["reward"] for s in samples], dtype=np.float)
-        dones = np.array([s["dones"] for s in samples], dtype=np.bool)
-        next_observations = np.stack([s["next_observation"] for s in samples], axis=0)
+        observations = np.stack([s.observation for s in samples], axis=0)
+        actions = np.array([s.action for s in samples], dtype=np.int)
+        rewards = np.array([s.reward for s in samples], dtype=np.float)
+        dones = np.array([s.done for s in samples], dtype=np.bool)
+        next_observations = np.stack([s.next_observation for s in samples], axis=0)
 
         return self.sample_batch(
             observations, actions, rewards, next_observations, dones
@@ -48,7 +50,6 @@ class UniformReplayBuffer(ReplayBufferInterface):
             "Transition",
             ["observation", "action", "reward", "next_observation", "done"],
         )
-        self._uniform_probabilities = np.full(fill_value=1 / buffer_size, shape=buffer_size)
 
     def add(self, observation, action, reward, next_obs, done):
         self.buffer.append(self.transition(observation, action, reward, next_obs, done))
@@ -58,4 +59,6 @@ class UniformReplayBuffer(ReplayBufferInterface):
         return self.transition_
 
     def probabilities(self):
-        return self._uniform_probabilities
+        return np.full(
+            fill_value=1 / len(self.buffer), shape=len(self.buffer)
+        )
